512483c47ac09b0af5190c172fc1400d1b611c43
Me <me@pop-os.localdomain>
2019-10-24 20:25:14 +0200

    Fixed Index

---
+++ src/components/index/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+      <h1>{{ msg }}</h1>
+      <p>
+          Whelcome to Kuinxe
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style lang="scss">
+h1 {
+    margin: 40px 0 0;
+    color: #42b983;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+      <Index msg="INDEX" />
+  </div>
+</template>
+
+<script>
+import Index from '@/components/index/Index.vue'
+export default {
+    name: 'index',
+    components: {
+        Index
+    }
+}
+</script>
