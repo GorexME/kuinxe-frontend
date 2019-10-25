806c229cb3c912e97cb3faa7cc9112e6ed7c391f
Daniel Grigore <daniel.grigore@fortnox.se>
2019-10-25 15:19:13 +0200

    Styalised about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,13 @@
   <div class="about">
     <h1>Contact</h1>
     <a href="https://www.facebook.com/kuinxe">facebook</a>
+    <br>
     <a href="https://www.instagram.com/kuinxe">instagram</a>
   </div>
 </template>
+<style lang="scss" scoped>
+h1,a {
+  padding: 15px;
+  text-decoration: underline  
+}
+</style>
